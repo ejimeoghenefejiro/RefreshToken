2bc3c2a85c8cfa48eb68263041b406072d253a65
Ejime Macdonald Oghenefejiro <53091656+ejimeoghenefejiro@users.noreply.github.com>
2021-06-09 17:57:54 +0100

    New update to main c

--- Main.c
+++ Main.c
@@ -6,5 +6,7 @@ int main()
      print("Result: %d", subtract(5 +2 ));
       print("Result: %d", multiply(5 +2 ));
      print("Result: %d", Divide(5 +2 ));
+       print("Result: %d", dd(5 +5 ));
+    
     return 0;
 }
